f830cede433b44706bcd30040fdf91cec8d032e0
ntl <nathanladd+github@gmail.com>
2016-01-21 12:51:24 -0600

    Rename counter to iterations

--- test/spec/server.rb
+++ test/spec/server.rb
@@ -2,14 +2,14 @@ require_relative './spec_init'
 
 describe 'Server' do
   specify 'Request and Response' do
-    counter = (ENV['COUNTER'] || '1').to_i
+    iterations = (ENV['COUNTER'] || '1').to_i
 
     HTTP::Server::Controls::ExampleServer.run do |port, scheduler|
       uri = URI::HTTP.build :host => 'localhost', :port => port, :path => '/countdown'
       connection = HTTP::Commands::Connect.(uri, scheduler: scheduler)
 
-      while counter > 0
-        entity = { 'counter' => counter }
+      while iterations > 0
+        entity = { 'iterations' => counter }
         json = JSON.pretty_generate entity
 
         response = HTTP::Commands::Post.(json, uri, connection: connection)
@@ -18,7 +18,7 @@ describe 'Server' do
         response = HTTP::Commands::Get.(uri, connection: connection)
         entity = JSON.parse response.body
 
-        counter = entity['counter']
+        iterations = entity['counter']
       end
     end
   end
